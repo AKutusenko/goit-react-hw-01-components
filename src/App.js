d33d28830e77cff4d5bb0ef41241be710dbf570f
Artur Kutusenko <kutusenko@gmail.com>
2021-08-15 23:59:21 +0300

    add task1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Profile from './components/profile/Profile';
+import user from './components/profile/user.json';
+
+export default function App() {
+  return (
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+}
