a4a692283185312476c2cf4c1af89bbcadb58593
Artur Kutusenko <kutusenko@gmail.com>
2021-08-20 07:24:56 +0300

    add task4 markup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Statistics from './components/statistics/Statistics';
 import statisticalData from './components/statistics/statistical-data.json';
 import FriendList from './components/friend-list/FriendList';
 import friendListData from './components/friend-list/friends.json';
+import TransactionHistory from './components/transaction-history/TransactionHistory';
+import transactionData from './components/transaction-history/transactions.json';
 
 export default function App() {
   return (
@@ -18,6 +20,7 @@ export default function App() {
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />;
       <FriendList friends={friendListData} />,
+      <TransactionHistory items={transactionData} />
     </div>
   );
 }
---
+++ src/components/transaction-history/TransactionHistory.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+
+export default function TransactionHistory({ items }) {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => {
+          return (
+            <tr key={item.id}>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
+
+TransactionHistory.defaultProps = {
+  type: 'Type not found',
+  amount: 'Amount not found',
+  currency: 'Currency not found',
+};
